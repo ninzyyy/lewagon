0baf9db37acfe6ef6e5e26a502166638051e63c9
Ninaad Kalla <ninaadkalla@gmail.com>
2023-01-08 03:37:18 +0530

    Fix style issues, should get a 'Good Style' now.

--- today.py
+++ today.py
@@ -1,8 +1,9 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
+
 def my_name_is():
+    """A function that defines my name."""
     return "ninzyyy"
 
 def my_buddy_is():
+    """A function that defines my buddy's name."""
     return "ninzyyy"
