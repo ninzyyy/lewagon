e7a5f45a2db3e110830b8f82aa850f1d80bf6568
Ninaad Kalla <ninaadkalla@gmail.com>
2023-01-11 18:55:37 +0530

    used environment variables (os.getenv) to return a respone

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,11 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    if os.getenv('FLASK_ENV') == 'development':
+        return "Starting in development mode..."
+    if os.getenv('FLASK_ENV') == 'production':
+        return "Starting in production mode..."
+    return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
