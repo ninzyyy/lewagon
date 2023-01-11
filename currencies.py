39fe71c08e1c859637d79b6493763229d2dac063
Ninaad Kalla <ninaadkalla@gmail.com>
2023-01-11 06:31:35 +0530

    created convert func, added currency rates, 8.57/10

--- currencies.py
+++ currencies.py
@@ -1,11 +1,15 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {"USDEUR" : 0.85, "GBPEUR" : 1.13, "CHFEUR" : 0.86, "EURGBP" : 0.885, "USDINR" : 81.69}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    conversion_string = amount[1] + currency
+    if conversion_string not in RATES:
+        return None
+    converted_amount = round(RATES[conversion_string] * amount[0])
+    return converted_amount
