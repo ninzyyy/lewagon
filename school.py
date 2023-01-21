6cd98f79743dd16276f7953f696ac0b8896c3849
Ninaad Kalla <ninaadkalla@gmail.com>
2023-01-21 09:17:26 -0500

    Competed warm-up task.

--- school.py
+++ school.py
@@ -2,14 +2,20 @@
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
 
+    query = """
+    SELECT * FROM students s
+    WHERE s.birth_city = ?
+    """
+    db.execute(query, [city])
+    rows = db.fetchall()
+    return rows
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+
+#import sqlite3
+#conn = sqlite3.connect('data/school.sqlite')
+#db = conn.cursor()
+#print(students_from_city(db, 'Paris'))
