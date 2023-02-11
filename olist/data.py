08ff17329da0e698c70cc1967e1e39d16e173637
Ninaad Kalla <ninaadkalla@gmail.com>
2023-02-11 15:22:11 -0500

    Completed get_data step

--- olist/data.py
+++ olist/data.py
@@ -14,7 +14,22 @@ class Olist:
             # Use __file__ instead as an absolute path anchor independant of your usename
             # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+
+        csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data/csv")
+        file_names = os.listdir(csv_path)
+        file_names.remove(".keep")
+
+        key_names = file_names
+        key_names = [file.replace(".csv", "").replace("_dataset","").replace("olist_","") for file in key_names]
+
+        dataframes = []
+        for file in file_names:
+            df = pd.read_csv(os.path.join(csv_path, file))
+            dataframes.append(df)
+            print(df)
+
+        data = dict(zip(key_names, dataframes))
+        return data
 
     def ping(self):
         """
